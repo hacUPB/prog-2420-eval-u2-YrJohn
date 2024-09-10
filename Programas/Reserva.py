224e8af63de9ca764b25939a3a7048774fdc7ed8
John <john.sinclair@upb.edu.co>
2024-09-10 16:53:40 -0500

    Verificacion de que si funciona el codigo que llevo hasta ahora

--- Programas/Reserva.py
+++ Programas/Reserva.py
@@ -18,7 +18,19 @@ precio = 79900 if dia_semana in ["lunes", "martes", "miércoles", "jueves"] and 
         119900 if dia_semana not in ["lunes", "martes", "miércoles", "jueves"] and distancia < 400 else \
         156900 if dia_semana in ["lunes", "martes", "miércoles", "jueves"] else 213000
 
-#Asiento
-preferencia = input("Prefiere asiento en el pasillo, ventana o no tiene preferencia? ")
-asiento = "C" if preferencia == "pasillo" else "A" if preferencia == "ventana" else "B"
-numero_asiento = random.randint(1, 29)
+
+
+
+
+
+
+
+
+#Reserva
+print(f"""
+Aqui esta su reserva:
+Nombre: {Nombre}
+origen: {origen}
+destino: {destino}
+fecha: {dia_semana} {dia_mes}
+precio: ${precio}""")
